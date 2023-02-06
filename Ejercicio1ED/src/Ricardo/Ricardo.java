2f6bb6ddab55da38f7fe45ffb040a16104cd694b
zarateh <ricardo.dromero89gmail.com>
2023-02-06 17:13:31 +0100

    Añadida clase Ricardo

---
+++ Ejercicio1ED/src/Ricardo/Ricardo.java
@@ -0,0 +1,8 @@
+package Ricardo;
+
+public class Ricardo {
+    public static void main (String [] args) {
+		System.out.println("Hola soy Ricardo");
+		System.out.println("Mis tres hobbies favoritos son: ver fútbol, ver tenis, ver fórmula 1");
+    }
+}
